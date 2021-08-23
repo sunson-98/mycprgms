abb5f2444092bebec02097dab672a6439875b9ed
bhagavan <bhagavan@auranetworks.in>
2021-08-23 22:49:32 +0530

    Fixing all issues and optimizing code

--- strings/main.c
+++ strings/main.c
@@ -1,92 +1,178 @@
 #include<stdio.h>
-int aislower (int c);
-int aisupper(int c);
-int aisdight(int c);
-int aisalnum(int c);
-int aisalpha(int c);
-int aiscntrl(int c);
-int aisgraph(int c);
-int aispunct(int c);
-int aisspace(int c);
-int aisascii(int c);
-int aisblank(int c);
-int aisxdight(int c);
-int astrlen(char *s);
-int atoupper(int c);
-int main()
+#include "defs.h"
+
+int test_islower()
+{
+	int f = 0;
+	char ch = 'a';
+
+	f = aislower(ch);
+	if (f == 0)
+		printf("%c: is NOT Lower\n", ch);
+	else 
+		printf("%c: is a Lower\n", ch);
+}
+
+int test_isupper()
+{
+	int f = 0;
+	char ch = 'a';
+
+	f = aisupper(ch);
+	if (f == 1)
+		printf("%c: is a UPPER\n", ch);
+	else 
+		printf("%c: is NOT UPPER\n", ch);
+}
+
+int test_isdegit()
+{
+	int f = 0;
+	char ch = 'a';
+
+	f = aisdight(ch);
+	if (f == 1)
+		printf("%c: is a degit\n", ch);
+	else 
+		printf("%c: is NOT degit\n", ch);
+}
+
+int test_isalnum()
+{
+	int f = 0;
+	char ch = 'a';
+
+	f = aisalnum(ch);
+	if (f == 1)
+		printf("%c: \n \n is alpha or numerical value",ch);
+	else 
+		printf("it is neither alpha value nor numerical value");
+}
+
+int test_isalpha()
 {
 	char ch = 'a';
 	int f;
-	//char str[100] = "aura networks";
+	f = aisalpha(ch);
+	if (f == 1)
+		printf("%c: \n \n is an alphabet", ch);
+	else 
+		printf ("%c: \n \n is not an alphabet", ch);
+}
 
-	//f = aislower(ch);
-//	if (f == 0)
-//		printf("%c: is NOT Lower\n", ch);
-//	else 
-//		printf("%c: is a Lower\n", ch);
+int test_iscntrl()
+{
+	char ch = 'a';
+	int f;
+	f = aiscntrl(ch);
+	if (f == 1)
+		printf("\n %c: \n \n is a control charecter", ch);
+	else
+		printf("\n %c: \n \n is not a control charecter", ch);
+}
 
-//	f = aisupper(ch);
-//	if (f == 1)
-//		printf("%c: is a UPPER\n", ch);
-//	else 
-//		printf("%c: is NOT UPPER\n", ch);
-    //f = aisdight(ch);
-    //if (f == 1)
-	//	printf("%d: is a dight\n", ch);
-	//else
-	//	printf("%c: is not a dight\n", ch);
-	//f = aisalnum(ch);
-	//if (f == 1)
-	//	printf("%c: \n \n is alpha or numerical value",ch);
-	//else 
-		//printf("it is neither alpha value nor numerical value");
-		//f = aisalpha(ch);
-		//if (f == 1)
-		//	printf("%c: \n \n is an alphabet", ch);
-		//else 
-			//printf ("%c: \n \n is not an alphabet", ch);
-			//f = aiscntrl(ch);
-			//if (f == 1)
-			//	printf("\n %c: \n \n is a control charecter", ch);
-			//else printf("\n %c: \n \n is not a control charecter", ch);
-			//f = aisgraph(ch);
-			//if (f == 1)
-			  //  printf("\n \n %c: \n \n is a printable charecter", ch);
-            //else 
-				//printf("\n \n %c: \n \n is not a printable charecter", ch);
-		      //f = aispunct(ch);
-			  //if (f == 1)
-				  //printf("\n \n %c: \n \n is punctuation charecter", ch);
-			  //else
-				  //printf("\n \n %c: \n \n is not a puntuation charecter", ch);
-         //  f = isspace(ch);
-		   //if (f == 0)
-	//		   printf("\n \n %c: \n \n is a space", ch);
-		//   else
-		//	   printf("\n \n %c: \n \n is not a space", ch);
-     
-//       f = aisascii(ch);
-//        if (f == 1)
-//			printf("\n \n %c: \n \n is an ascii value", ch);
-//			printf("\n \n %c: \n \n is an ascii value", ch);
-//		else
-//			printf("\n \n %c: \n \n is not an ascii value", ch);
-  //       f = aisblank(ch);
-//		 if (f == 1)
-//			 printf("\n \n %c: \n \n is blank space", ch);
-//		 else 
-//			 printf("\n \n %c: \n \n is not blank space", ch);
-//     f = aisxdight(ch);
-//	 if (f == 1)
-//		 printf("\n \n %c: \n \n is an hexa decimal num ", ch);
-//	 else
-//		 printf("\n \n %c: \n \n is not an hexa decimal num", ch);
-         //f = astrlen(str);
+int test_isgraph()
+{
+	char ch = 'a';
+	int f;
+	f = aisgraph(ch);
+	if (f == 1)
+		printf("\n \n %c: \n \n is a printable charecter", ch);
+	else 
+		printf("\n \n %c: \n \n is not a printable charecter", ch);
+}
 
-		   //   printf("\n \n %d \n \nlen = ", f);
-		   f = atoupper(ch);
-		   printf("\n \n %c: \n \n ",ch);
-		 
+int test_ispunct()
+{
+	char ch = 'a';
+	int f;
+	f = aispunct(ch);
+	if (f == 1)
+		printf("\n \n %c: \n \n is punctuation charecter", ch);
+	else
+		printf("\n \n %c: \n \n is not a puntuation charecter", ch);
+}
+
+int test_isspace()
+{
+	char ch = 'a';
+	int f;
+	f = aisspace(ch);
+	if (f == 0)
+		printf("\n \n %c: \n \n is a space", ch);
+	else
+		printf("\n \n %c: \n \n is not a space", ch);
+}
+
+int test_isasscii()
+{
+	char ch = 'a';
+	int f;
+	f = aisascii(ch);
+	if (f == 1)
+		printf("\n \n %c: \n \n is an ascii value", ch);
+	else
+		printf("\n \n %c: \n \n is not an ascii value", ch);
+}
+
+int test_isblank()
+{
+	char ch = 'a';
+	int f;
+	f = aisblank(ch);
+	if (f == 1)
+		printf("\n \n %c: \n \n is blank space", ch);
+	else 
+		printf("\n \n %c: \n \n is not blank space", ch);
+}
+
+int test_isxdigit()
+{
+	char ch = 'a';
+	int f;
+	f = aisxdight(ch);
+	if (f == 1)
+		printf("\n \n %c: \n \n is an hexa decimal num ", ch);
+	else
+		printf("\n \n %c: \n \n is not an hexa decimal num", ch);
+}
+
+int test_astrlen()
+{
+	char str[100] = "aura networks";
+	int f;
+	f = astrlen(str);
+	printf("%s len :%d\n", str, f);
+}
+
+int test_atoupper()
+{
+	char retval;
+	char ch = 'a';
+
+	retval = atoupper(ch);
+	printf("After converting %c to UPPER :%c\n", ch, retval);
+}
+
+int main()
+{
+	/*
+	test_islower();
+	test_isupper();
+	test_isdegit();
+	test_isalnum();
+	test_isalpha();
+	test_isalpha();
+	test_iscntrl();
+	test_isgraph();
+	test_ispunct();
+	test_isspace();
+	test_isasscii();
+	test_isblank();
+	test_isxdigit();
+	test_astrlen();
+	*/
+	test_atoupper();
 }
 
 

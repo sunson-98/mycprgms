8d5d63b64c58b3b5368d20ffb1e404b5182cd8bf
bhagavan <bhagavan@auranetworks.in>
2021-09-02 07:39:39 -0400

    adding double list

---
+++ ll/doublelist.c
@@ -0,0 +1,165 @@
+#include"stdio.h"
+#include"stdlib.h"
+struct student
+{
+	int no;
+	struct student *next;
+	struct student *p;
+};
+
+struct student *h;
+
+int add_atbegin(int v)
+{
+	struct student *p;
+	p = (struct student*) malloc(sizeof(struct student));
+	p -> no = v;
+	p -> next = h;
+	h = p;
+}
+
+
+int get_count()
+{
+	struct student *t;
+	int i = 0;
+	t = h;
+	while(t != NULL)
+	{
+		t = t -> next;
+		i = i + 1;
+	}
+	printf("\nno of nodes = %d\n", i);
+}
+
+int valbypos(int x)
+{
+	int i = 0;
+	struct student *t =h;
+	while(i != x)
+	{
+		t = t -> next;
+		i = i + 1;
+	}
+	return (t -> no);
+}
+
+int posbyval(int v)
+{
+
+	int i = 0;
+	struct student *t = h;
+
+	while(t -> no != v)
+	{
+		t = t -> next;
+		i = i + 1;
+	}
+	return i;
+}
+
+  int del_nodebyval(int v)
+{
+	struct student *t = h;
+	
+	while(t -> next -> no != v)
+	{
+		t = t -> next;
+	}
+	t -> next = t -> next -> next;
+	return 0;
+}
+
+  int del_nodebypos(int x)
+{
+	struct student *t = h;
+	int i = 0;
+	while(i != x)
+	{
+		t = t -> next;
+		i = i + 1;
+	}
+	 t -> next = t -> next -> next;
+	 return 0;
+}
+int add_node_double(int v)
+{
+	struct student *p = NULL, *t = NULL;
+	p = (struct student *) malloc(sizeof(struct student));
+	t = h;
+	p -> no = v;
+	p -> next = NULL;
+	if (h == NULL)
+	{
+		h = p;
+		p -> p = NULL;
+		return 0;
+	}
+	while(t -> next != NULL)
+	{
+		t = t -> next;
+	}
+
+	  t -> next = p;
+	  p -> p = t;
+
+	  return 0;
+}
+
+ int valbypos(int x);
+int getmid_val()
+{
+	struct student *t = h;
+	int y, i = 0;
+
+	while(t -> next != NULL)
+	{
+		t = t -> next;
+		i = i + 1;
+	}
+    
+	y = i / 2;
+	printf("\n mid value = %d\n", valbypos(y));
+}
+
+		
+
+
+
+int dumplist()
+{
+	struct student *t = h;
+	
+	while(t != NULL)
+	{
+		printf("\nnode value = %d\n", t -> no);
+		t = t->next;
+	}
+}
+
+int main()
+{
+	int y;
+	int l;
+	add_node_double(10);
+	add_node_double(20);
+	add_node_double(30);
+	add_node_double(50);
+	add_atbegin(100);
+	dumplist();
+    getmid_val();
+	get_count();
+	y = valbypos(3);
+	l = posbyval(30);
+	
+
+
+	printf("\nvalue by given position = %d\n", y);
+	printf("\nposition by given value = %d\n", l);	
+}
+
+
+
+
+
+
